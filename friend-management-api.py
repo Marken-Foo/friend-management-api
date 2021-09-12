40eb84d0c72393667f0ae4e1f84d9c8e68bc7e74
Marken <>
2021-09-13 00:40:13 +0800

    Added endpoint to add friend connection

--- friend-management-api.py
+++ friend-management-api.py
@@ -15,7 +15,7 @@ def connect_to_db(db_path=DB_NAME):
     return con
 
 
-def create_json_response(is_success, **kwargs):
+def create_json_response(is_success=False, **kwargs):
     return jsonify({"success": is_success, **kwargs})
 
 
@@ -26,6 +26,32 @@ def is_email_valid(email_str):
     return match is not None
 
 
+def are_users_friends(conn, email1, email2):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM friend "
+        "INNER JOIN email AS e1 ON friend.email_id1 = e1.email_id "
+        "INNER JOIN email AS e2 ON friend.email_id2 = e2.email_id "
+        "WHERE e1.email = ? AND e2.email = ? "
+        ";", (email1, email2)
+    )
+    res = cur.fetchone() is not None
+    cur.close()
+    return res
+
+
+def are_users_blocking(conn, email1, email2):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM block "
+        "INNER JOIN email AS e1 ON block.blocker_email_id = e1.email_id "
+        "INNER JOIN email AS e2 ON block.blocked_email_id = e2.email_id "
+        "WHERE e1.email = ? AND e2.email = ? "
+        ";", (email1, email2)
+    )
+    res = cur.fetchone() is not None
+    cur.close()
+    return res
+
+
 @app.post("/users")
 def add_email():
     req = request.get_json()
@@ -33,15 +59,15 @@ def add_email():
         email = req["email"]
     except KeyError:
         return create_json_response(is_success=False,
-            error="No email address received"
+            error="No email address received (JSON key should be 'email')"
         )
     if not is_email_valid(email):
         return create_json_response(is_success=False,
             error="Invalid email address received"
         )
-    conn = connect_to_db()
-    cur = conn.cursor()
     try:
+        conn = connect_to_db()
+        cur = conn.cursor()
         cur.execute("INSERT INTO email (email) VALUES (?);", (email,))
         conn.commit()
         return create_json_response(is_success=True)
@@ -61,11 +87,51 @@ def add_email():
 
 @app.post("/friend")
 def add_friends():
-    # check length is 2, check if are emails, if both in db
-    # if blocked, respond
-    # if already friends, respond
-    # else update db and respond
-    return "add friends"
+    req = request.get_json()
+    try:
+        emails = req["friends"]
+    except KeyError:
+        return create_json_response(is_success=False,
+            error="No email addresses received (JSON key should be 'friends', "
+            "email addresses should be in array value)"
+        )
+    if ((len(emails) < 2)
+        or emails[0] == emails[1]
+        or (not is_email_valid(emails[0]))
+        or (not is_email_valid(emails[1]))
+    ):
+        return create_json_response(is_success=False,
+            error="Two distinct valid email addresses required"
+        )
+    with connect_to_db() as conn:
+        # if blocked, respond
+        if are_users_blocking(conn, emails[0], emails[1]):
+            return create_json_response(is_success=False,
+                error="At least one user is blocking the other"
+            )
+        # if already friends, respond
+        if are_users_friends(conn, emails[0], emails[1]):
+            return create_json_response(is_success=False,
+                error="Users are already friends"
+            )
+        else:
+            try:
+                cur = conn.cursor()
+                cur.execute("INSERT INTO main.friend (email_id1, email_id2) "
+                    "SELECT e1.email_id, e2.email_id "
+                    "FROM email AS e1 "
+                    "INNER JOIN email AS e2 "
+                    "ON e1.email = ? AND e2.email = ? "
+                    ";", (emails[0], emails[1])
+                )
+                conn.commit()
+                return create_json_response(is_success=True)
+            except sqlite3.IntegrityError as err:
+                message = err.args[0]
+                # We had checked for existing friend connection already
+                return create_json_response(is_success=False,
+                    error="Unexpected SQLite integrity error"
+                )
 
 
 @app.post("/unfriend")
